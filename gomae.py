2ceb2443c9089c6d26b2d8d3507bd34f65580c97
YuZakuro <zakuro@yuzakuro.me>
2015-06-15 09:54:49 +0900

    ログをtweet

--- gomae.py
+++ gomae.py
@@ -25,16 +25,27 @@ if __name__ == '__main__':
     
     target_time = local_time + timedelta(minutes=settings.wait_minutes)
     target_time = datetime(*(target_time.timetuple()[:5]), tzinfo=timezone.utc)
-    print('目標時刻 %s' % target_time.isoformat())
+    
+    messages = ['ごまえー♪ログ']
+
+    messages.append('目標時刻 %s' % target_time.isoformat())
+    print(messages[-1])
     
     server_time = dateutil.parser.parse(tweet(settings.pre_text)['created_at'])
     delta = server_time - local_time
-    print('時刻誤差 %s' % subtract_min_from_max(server_time, local_time))
+    messages.append('時刻誤差 %s' % subtract_min_from_max(server_time, local_time))
+    print(messages[-1])
 
     wait_seconds = (target_time - datetime.now(timezone.utc) - delta).seconds
-    print('待機時間 %s秒' % wait_seconds)
+    messages.append('待機時間 %s秒' % wait_seconds)
+    print(messages[-1])
+
     time.sleep(wait_seconds + settings.tolerance_seconds)
     
     tweeted_time = dateutil.parser.parse(tweet(settings.text)['created_at'])
-    print('tweet時刻 %s' % (tweeted_time.isoformat())) 
-    print('誤差 %s' % subtract_min_from_max(tweeted_time, target_time)) 
+    messages.append('tweet時刻 %s' % (tweeted_time.isoformat()))
+    print(messages[-1])
+    messages.append('誤差 %s' % subtract_min_from_max(tweeted_time, target_time))
+    print(messages[-1])
+
+    tweet('\n'.join(messages))
